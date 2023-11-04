79bfc5c566e424005362ae4d8dc6b1cbd2af9976
Mfaume Mnokote <mmnokote@gmail.com>
2023-11-04 12:44:48 +0300

    updates in orm config

--- src/orm.config.ts
+++ src/orm.config.ts
@@ -1,29 +1,12 @@
-// import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-// // import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
-// const config: TypeOrmModuleOptions = {
-//   type: 'postgres',
-//   username: 'postgres',
-//   password: 'p@ssw0rd',
-//   port: 5432,
-//   host: 'localhost',
-//   database: 'nestjsm0',
-//   synchronize: true,
-//   migrations: ['dist/db/migrations/*.js'],
-//   cli: { migrationsDir: 'src/db/migrations' },
-
-//   entities: ['dist/**/*.entity{.ts,.js}'],
-// };
-// export default config;
-
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 // import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 const config: TypeOrmModuleOptions = {
   type: 'postgres',
   username: 'postgres',
-  password: 'P@ssw0rd@098',
+  password: 'p@ssw0rd',
   port: 5432,
   host: 'localhost',
-  database: 'bonde',
+  database: 'nestjsm0',
   synchronize: true,
   migrations: ['dist/db/migrations/*.js'],
   cli: { migrationsDir: 'src/db/migrations' },
@@ -31,3 +14,20 @@ const config: TypeOrmModuleOptions = {
   entities: ['dist/**/*.entity{.ts,.js}'],
 };
 export default config;
+
+// import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+// // import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+// const config: TypeOrmModuleOptions = {
+//   type: 'postgres',
+//   username: 'postgres',
+//   password: 'P@ssw0rd@098',
+//   port: 5432,
+//   host: 'localhost',
+//   database: 'bonde',
+//   synchronize: true,
+//   migrations: ['dist/db/migrations/*.js'],
+//   cli: { migrationsDir: 'src/db/migrations' },
+
+//   entities: ['dist/**/*.entity{.ts,.js}'],
+// };
+// export default config;
