7492bf5ca77fdc2f1a2f6d4744c2358909bc0e03
Mfaume Mnokote <mmnokote@gmail.com>
2024-02-29 13:02:52 +0300

    updates_on_subthemes2

--- src/orm.config.ts
+++ src/orm.config.ts
@@ -1,6 +1,6 @@
 // git rm --cached src/orm.config.ts
 
-// import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 // const config: TypeOrmModuleOptions = {
 //   type: 'postgres',
@@ -15,33 +15,33 @@
 //   entities: ['dist/**/*.entity{.ts,.js}'],
 // };
 
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-const config: TypeOrmModuleOptions = {
-  type: 'postgres',
-  username: 'postgres',
-  password: 'p@ssw0rd',
-  port: 5432,
-  host: 'localhost',
-  database: 'confecence',
-  synchronize: true,
-  migrations: ['dist/db/migrations/*.js'],
-  cli: { migrationsDir: 'src/db/migrations' },
-
-  entities: ['dist/**/*.entity{.ts,.js}'],
-};
-
+// import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 // const config: TypeOrmModuleOptions = {
-//   type: 'mysql', // Change 'postgres' to 'mysql'
+//   type: 'postgres',
+//   username: 'postgres',
+//   password: 'p@ssw0rd',
+//   port: 5432,
 //   host: 'localhost',
-//   port: 3306, // Default MySQL port
-//   username: 'root', // Your MySQL username
-//   password: 'root', // Your MySQL password
-//   database: 'conference', // Your MySQL database name
+//   database: 'confecence',
 //   synchronize: true,
 //   migrations: ['dist/db/migrations/*.js'],
 //   cli: { migrationsDir: 'src/db/migrations' },
+
 //   entities: ['dist/**/*.entity{.ts,.js}'],
 // };
+
+const config: TypeOrmModuleOptions = {
+  type: 'mysql', // Change 'postgres' to 'mysql'
+  host: 'localhost',
+  port: 3306, // Default MySQL port
+  username: 'root', // Your MySQL username
+  password: 'root', // Your MySQL password
+  database: 'conference', // Your MySQL database name
+  synchronize: true,
+  migrations: ['dist/db/migrations/*.js'],
+  cli: { migrationsDir: 'src/db/migrations' },
+  entities: ['dist/**/*.entity{.ts,.js}'],
+};
 // export default config;
 
 // import { TypeOrmModuleOptions } from '@nestjs/typeorm';
