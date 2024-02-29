bba40eb41c1b905c67856e9c8fefd093ef1ee18c
Mfaume Mnokote <mmnokote@gmail.com>
2024-02-29 13:55:07 +0300

    updates_on_subthemes2

--- src/orm.config.ts
+++ src/orm.config.ts
@@ -2,18 +2,18 @@
 
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-// const config: TypeOrmModuleOptions = {
-//   type: 'postgres',
-//   username: 'postgres',
-//   password: 'ABCtscm1s@@2023', // Update with the correct password
-//   port: 5432,
-//   host: '172.16.18.193',
-//   database: 'conferencelive', // Corrected database name
-//   synchronize: true,
-//   migrations: ['dist/db/migrations/*.js'],
-//   cli: { migrationsDir: 'src/db/migrations' },
-//   entities: ['dist/**/*.entity{.ts,.js}'],
-// };
+const config: TypeOrmModuleOptions = {
+  type: 'postgres',
+  username: 'postgres',
+  password: 'ABCtscm1s@@2023', // Update with the correct password
+  port: 5432,
+  host: '172.16.18.193',
+  database: 'conferencelive', // Corrected database name
+  synchronize: true,
+  migrations: ['dist/db/migrations/*.js'],
+  cli: { migrationsDir: 'src/db/migrations' },
+  entities: ['dist/**/*.entity{.ts,.js}'],
+};
 
 // import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 // const config: TypeOrmModuleOptions = {
@@ -30,18 +30,18 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 //   entities: ['dist/**/*.entity{.ts,.js}'],
 // };
 
-const config: TypeOrmModuleOptions = {
-  type: 'mysql', // Change 'postgres' to 'mysql'
-  host: 'localhost',
-  port: 3306, // Default MySQL port
-  username: 'root', // Your MySQL username
-  password: 'root', // Your MySQL password
-  database: 'conference', // Your MySQL database name
-  synchronize: true,
-  migrations: ['dist/db/migrations/*.js'],
-  cli: { migrationsDir: 'src/db/migrations' },
-  entities: ['dist/**/*.entity{.ts,.js}'],
-};
+// const config: TypeOrmModuleOptions = {
+//   type: 'mysql', // Change 'postgres' to 'mysql'
+//   host: 'localhost',
+//   port: 3306, // Default MySQL port
+//   username: 'root', // Your MySQL username
+//   password: 'root', // Your MySQL password
+//   database: 'conference', // Your MySQL database name
+//   synchronize: true,
+//   migrations: ['dist/db/migrations/*.js'],
+//   cli: { migrationsDir: 'src/db/migrations' },
+//   entities: ['dist/**/*.entity{.ts,.js}'],
+// };
 // export default config;
 
 // import { TypeOrmModuleOptions } from '@nestjs/typeorm';
